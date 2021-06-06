4145099cee6e68bfaba20a0bc6cc0f11427e9737
Kalan Miurrelle <Kalan.miurrelle@gmail.com>
2021-06-06 04:16:34 -0700

    Add color to affinity chart

--- scripts/affinityChart.js
+++ scripts/affinityChart.js
@@ -2,13 +2,14 @@ const c = document.getElementById("affinityChart");
 const ctx = c.getContext('2d');
 
 const a = 2 * Math.PI / 6;
-const bgRadius = (c.width / 2) - 10;
+const bgRadius = (c.width / 2) - 30;
 
-var hexPoints = [];
+
 var affinityPoints = [];
 
 function init()
 {
+    var hexPoints = [];
     clearCanvas();
     //draw hexagons
     for(var i = 0; i < 6; i++)
@@ -27,7 +28,15 @@ function init()
             affinityTemp.push(hexPoints[k][j]);
         }
         affinityPoints.push(affinityTemp);
-    } 
+    }
+    ctx.font = "12px Arial";
+    ctx.fillStyle = "#575757";
+    ctx.fillText("Vis", affinityPoints[0][0][0] - 10, affinityPoints[0][0][1] + 20);
+    ctx.fillText("Aqua", affinityPoints[1][0][0] + 10, affinityPoints[1][0][1] + 10);
+    ctx.fillText("Nox", affinityPoints[2][0][0] + 10, affinityPoints[2][0][1]);
+    ctx.fillText("Terra", affinityPoints[3][0][0] -12, affinityPoints[3][0][1] - 10);
+    ctx.fillText("Lux", affinityPoints[4][0][0] -32, affinityPoints[4][0][1]);
+    ctx.fillText("Ignis", affinityPoints[5][0][0] -32, affinityPoints[5][0][1] + 10);
 }
 
 function drawHexagon(x,y,r)
@@ -62,8 +71,17 @@ function clearCanvas()
 function drawChart(affinityValues)
 {
     var affinityMap = [];
+    var highestAffinity = 0;
     for(var i = 0; i < 6; i++)
     {
+        if(affinityValues[i] == "S")
+        {
+            highestAffinity = i;
+        }
+        if(affinityValues[i] < affinityValues[highestAffinity] && affinityValues[highestAffinity] != "S")
+        {
+            highestAffinity = i;
+        }
         switch(affinityValues[i])
         {
             case "S":
@@ -87,10 +105,7 @@ function drawChart(affinityValues)
             default:
                 console.log("something went wrong");
         }
-    }
-
-
-    
+    }  
     ctx.beginPath();
     ctx.moveTo(affinityMap[0][0], affinityValues[0][1]);
     for(var i = 0; i < affinityMap.length; i++)
@@ -98,8 +113,34 @@ function drawChart(affinityValues)
         ctx.lineTo(affinityMap[i][0], affinityMap[i][1]);
     }
     ctx.lineTo(affinityMap[0][0], affinityValues[0][1]);
-    
     ctx.closePath();
+    var grd = ctx.createRadialGradient(c.width / 2, c.height / 2, 0, c.width / 2, c.height / 2, bgRadius / 2);
+    grd.addColorStop(0, "FloralWhite");
+    switch(highestAffinity)
+    {
+        case 0:
+            grd.addColorStop(1, "SlateGrey");
+            break;
+        case 1:
+            grd.addColorStop(1, "Aqua")
+            break;
+        case 2:
+            grd.addColorStop(1, "RebeccaPurple");
+            break;
+        case 3:
+            grd.addColorStop(1, "OliveDrab");
+            break;
+        case 4:
+            grd.addColorStop(1, "Gold");
+            break;
+        case 5:
+            grd.addColorStop(1, "Tomato");
+            break;
+        default:
+            grd.addColorStop(1, "Black");
+    }
+    
+    ctx.fillStyle = grd;
     ctx.fill();
 }
 
